78758965227fe75724a87dcb27707b6fbfb49d00
una <husnafiryal.azzahra@gmail.com>
2025-12-15 06:16:53 +0700

    fixing supabase apu

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,4 @@
 // js/supabase.js
-
-// 1. Pastikan library Supabase sudah ter-load dari HTML
-if (typeof supabase === 'undefined') {
-    console.error('CRITICAL: Script Supabase belum dimuat di file HTML!');
-    alert('Gagal memuat sistem database. Cek koneksi internet atau script HTML.');
-}
-
 const supabaseUrl = 'https://ohbpqbhphpdlqzdnvtov.supabase.co';
 
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oYnBxYmhwaHBkbHF6ZG52dG92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2ODM0MTIsImV4cCI6MjA3OTI1OTQxMn0.wh_4j201Ci6C3cR-gCnwwe6mt3hyS_BAqs_7mExezqY';
