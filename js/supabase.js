e198ddccac05f39805ead0b148f7ac31f22f8d2e
una <husnafiryal.azzahra@gmail.com>
2025-12-15 06:12:16 +0700

    fixing supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -7,6 +7,7 @@ if (typeof supabase === 'undefined') {
 }
 
 const supabaseUrl = 'https://ohbpqbhphpdlqzdnvtov.supabase.co';
+
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9oYnBxYmhwaHBkbHF6ZG52dG92Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM2ODM0MTIsImV4cCI6MjA3OTI1OTQxMn0.wh_4j201Ci6C3cR-gCnwwe6mt3hyS_BAqs_7mExezqY';
 // Variable 'supabase' ini otomatis ada karena kita pasang script di HTML
 const client = supabase.createClient(supabaseUrl, supabaseKey);
